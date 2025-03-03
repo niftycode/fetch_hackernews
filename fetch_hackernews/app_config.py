a424466596a5d1c1cad45c694fb758724d567ff3
Bodo Schönfeld <bodo.schoenfeld@niftycode.de>
2025-03-03 16:46:54 +0100

    fix directory error

--- fetch_hackernews/app_config.py
+++ fetch_hackernews/app_config.py
@@ -6,11 +6,15 @@
 Check if the .config directory exists
 Python 3.12+
 Date created: January 31st, 2022
-Date modified: October 15th, 2022
+Date modified: March 3rd, 2025
 """
 
-import getpass
+# import getpass
 import os
+
+from os.path import expanduser
+
+DIRECTORY = expanduser("~") + "/.config"
 
 
 def check_config_dir():
@@ -19,19 +23,19 @@ def check_config_dir():
     Create .config and hackernews directories if they
     doesn't exist.
 
-    PATH: /Users/<username>/.config/hackernews
+    PATH: ~.config/hackernews
     """
-    config_directory = f"/Users/{getpass.getuser()}/.config"
-    directory = "hackernews"
-    parent_path = config_directory
-    hackernews_path = os.path.join(parent_path, directory)
+    # config_directory = f"/Users/{getpass.getuser()}/.config"
+    app_directory = "hackernews"
+    # parent_path = config_directory
+    hackernews_path = os.path.join(DIRECTORY, app_directory)
     mode = 0o755
 
     # Create .config and .config/hackernews directories if necessary
-    if os.path.isdir(config_directory):
+    if os.path.isdir(DIRECTORY):
         if not os.path.isdir(hackernews_path):
             os.mkdir(hackernews_path, mode)
     else:
-        config_dir_path = f"/Users/{getpass.getuser()}/.config"
-        os.mkdir(config_dir_path, mode)
+        # config_dir_path = f"/Users/{getpass.getuser()}/.config"
+        os.mkdir(DIRECTORY, mode)
         os.mkdir(hackernews_path, mode)
